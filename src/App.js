2ce68c620d6a1ed894f7c31f428cbfee1553cb46
threeBearsWithBike <trois@list.ru>
2024-02-04 20:37:05 +0300

    add question

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import questions from './questions';
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+
+  const handleQuizAgainButtonClick = () => {
+    setCurrentQuestion(0);
+    setShowScore(false);
+    setScore(0);
+  }
+
+  const handleAnswerButtonClick = (isCorrect) => {
+    if (isCorrect === true) {
+      setScore(score + 1);
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showScore ? (
+        <div className='score-section'>Ваш результат {score} из {questions.length}
+          <button onClick={handleQuizAgainButtonClick}>
+            Еще раз
+          </button>
+        </div>
+      ) : (
+        <>
+          <div className='question-section'>
+
+            <div className='question-count'>
+              <span>Вопрос {currentQuestion + 1}</span> / {questions.length}
+            </div>
+            <div className='question-text'>
+              {questions[currentQuestion].questionText}
+            </div>
+
+          </div>
+
+          <div className='answer-section'>
+              {questions[currentQuestion].answerOptions.map(answerOptions => {
+                return (
+                  <button onClick={() => handleAnswerButtonClick(answerOptions.isCorrect)}>
+                  {answerOptions.answerText}
+                  </button>
+                )
+              })
+            }
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/questions.js
@@ -0,0 +1,68 @@
+const questions = [
+    {
+      questionText: 'Из какой страны вернулся в Россию Лев Николаевич?',
+      answerOptions: [
+        { answerText: 'Голландия', isCorrect: false },
+        { answerText: 'Франция', isCorrect: false },
+        { answerText: 'Германия', isCorrect: false },
+        { answerText: 'Швейцария', isCorrect: true }
+      ]
+    },
+    {
+      questionText: 'Какую сумму одолжил генерал Иволгин у князя?',
+      answerOptions: [
+        { answerText: '5 рублей', isCorrect: false },
+        { answerText: '25 рублей', isCorrect: false },
+        { answerText: '10 рублей', isCorrect: false },
+        { answerText: '15 рублей', isCorrect: true }
+      ]
+    },
+    {
+      questionText: 'Какой подарок передала князю Аглая? ',
+      answerOptions: [
+        { answerText: 'Одекалон', isCorrect: false },
+        { answerText: 'Ежа', isCorrect: true },
+        { answerText: 'Вазу', isCorrect: false },
+        { answerText: 'Картину', isCorrect: false }
+      ]
+    },
+    {
+      questionText: 'Улица, на которой жил Рогожин?',
+      answerOptions: [
+        { answerText: 'наб. р. Фонтанки', isCorrect: false },
+        { answerText: 'Садовая', isCorrect: false },
+        { answerText: 'Гороховая', isCorrect: true },
+        { answerText: 'Невский пр.', isCorrect: false }
+      ]
+    },
+    {
+      questionText: 'Копия какой картины привлекла князя у Рогожина дома?',
+      answerOptions: [
+        { answerText: 'Ночной дозор', isCorrect: false },
+        { answerText: 'Мертвый Христос в гробу', isCorrect: true },
+        { answerText: 'Венера перед зеркалом', isCorrect: false },
+        { answerText: 'Святая Инесса и ангел', isCorrect: false }
+      ]
+    },
+    {
+      questionText: 'Кто украл 400 рублей у Лебедева?',
+      answerOptions: [
+        { answerText: 'Келлер', isCorrect: false },
+        { answerText: 'Бурдовский', isCorrect: false },
+        { answerText: 'Иволгин', isCorrect: true },
+        { answerText: 'Фердыщенко', isCorrect: false }
+      ]
+    },
+    {
+      questionText: 'Как Парфен убил Настасью Филипповну?',
+      answerOptions: [
+        { answerText: 'Зарезал', isCorrect: true },
+        { answerText: 'Отравил', isCorrect: false },
+        { answerText: 'Задушил', isCorrect: false },
+        { answerText: 'Застрелил', isCorrect: false },
+      ]
+    }
+  ];
+
+
+  export default questions;
